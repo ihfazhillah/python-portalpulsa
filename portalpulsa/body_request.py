4cd1e7d864aacef1d5109d681394522e173d84c5
ihfazhillah <mihfazhillah@gmail.com>
2017-02-23 12:14:05 +0700

    validasi inquiry berhasil

--- portalpulsa/body_request.py
+++ portalpulsa/body_request.py
@@ -1,9 +1,17 @@
 """body_request, class untuk data request yang akan dikirim ke server"""
-from portalpulsa.exceptions import ParamsNotFound
-
+from portalpulsa.exceptions import ParamsNotFound, InvalidInquiry
+from portalpulsa.inquiry import Inquiry
 
 class BodyRequestBase(object):
     """Base body_request"""
 
     def __init__(self, inquiry):
-        self.inquiry = inquiry
+        self.inquiry = self._validate_inquiry(inquiry)
+
+    @classmethod
+    def _validate_inquiry(cls, inquiry):
+        """memvalidasi inquiry, harus sesuai yang ada di 
+        class Inquiry"""
+        if not inquiry in Inquiry.__dict__.values():
+            raise InvalidInquiry('Inquiry salah')
+        return inquiry
--- tests/test_body_request_base.py
+++ tests/test_body_request_base.py
@@ -3,9 +3,20 @@ setiap request ke server portalpulsa.com"""
 
 from nose import tools
 from portalpulsa.body_request import BodyRequestBase
+from portalpulsa.exceptions import InvalidInquiry
+from portalpulsa.inquiry import Inquiry
 
 
 
 @tools.raises(TypeError)
 def test_bila_user_tidak_memasukkan_inquiry_param():
     BodyRequestBase()
+
+
+def test_validate_inquiry_benar():
+    body = BodyRequestBase(Inquiry.SALDO)
+    tools.eq_(body.inquiry, 'S')
+
+@tools.raises(InvalidInquiry)
+def test_validate_inquiry_salah():
+    body = BodyRequestBase('N')
