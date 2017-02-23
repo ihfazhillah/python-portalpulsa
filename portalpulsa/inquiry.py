b300bd98fe191b85768f47b700b1dcc5b9ad228b
ihfazhillah <mihfazhillah@gmail.com>
2017-02-23 11:34:35 +0700

    portalpulsa python wrapper

---
+++ portalpulsa/inquiry.py
@@ -0,0 +1,11 @@
+"""Memuat daftar inquiry yang falid"""
+
+
+class Inquiry(object):
+    """Inquiry object"""
+    DEPOSIT = 'D'
+    SALDO = 'S'
+    HARGA = 'C'
+    STATUS = 'STATUS'
+    PULSA = 'I'
+    PLN = 'PLN'
