650951fde848a88b06881f5e50b2799697cfef1d
ihfazhillah <mihfazhillah@gmail.com>
2017-02-23 13:21:10 +0700

    bank list

---
+++ portalpulsa/bank.py
@@ -0,0 +1,9 @@
+"""Module untuk bank """
+
+class Bank(object):
+    """Daftar bank yang disupport portalpulsa.com"""
+    MANDIRI = 'mandiri'
+    BCA = 'bca'
+    BNI = 'bni'
+    MUAMALAT = 'muamalat'
+    BRI = 'bri'
