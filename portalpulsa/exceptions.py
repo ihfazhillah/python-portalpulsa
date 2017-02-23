49190f3b0a0e7757db4052b835b578e46d8e582a
ihfazhillah <mihfazhillah@gmail.com>
2017-02-23 11:38:35 +0700

    menambahkan exceptions

---
+++ portalpulsa/exceptions.py
@@ -0,0 +1,22 @@
+"""Daftar custom exceptions"""
+
+
+class ParamsNotFound(Exception):
+    """Bila parameter yang dibutuhkan tidak diberikan"""
+    pass
+
+class InactiveAPI(Exception):
+    """Bila API tidak diaktifkan"""
+    pass
+
+class NotAllowedAPI(Exception):
+    """Bila API tidak terdaftar, API user yang melakukan request"""
+    pass
+
+class AuthError(Exception):
+    """Raised ketika user salah key,userid atau secret"""
+    pass
+
+class InvalidInquiry(Exception):
+    """Raised ketika user salah memasukkan inquiry"""
+    pass
